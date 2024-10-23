0ed0c4e3b7e929d662b4ab6b5e7f43af340ff221
Hulbert Alejandro <hulberta.arangof@uqvirtual.edu.co>
2024-10-23 12:29:59 -0500

    Registro componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./componentes/header/header.component";
+import { FooterComponent } from "./componentes/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'barber-shop';
+  footer = 'Universidad del Quindío - 2024-2'
 }
---
+++ src/app/componentes/registro/registro.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControlOptions, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-registro',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent {
+
+  registroForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { 
+    this.crearFormulario();
+  }
+
+  private crearFormulario() {
+    this.registroForm = this.formBuilder.group({
+      nombre: ['', [Validators.required]],
+      telefono: ['', [Validators.required, Validators.maxLength(10)]],
+      correo: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(10)]],
+      confirmaPassword: ['', [Validators.required]]
+    }, { validators: this.passwordsMatchValidator } as AbstractControlOptions);
+  }
+
+  // Validador personalizado para confirmar que las contraseñas coincidan
+  passwordsMatchValidator(formGroup: FormGroup) {
+    const password = formGroup.get('password')?.value;
+    const confirmaPassword = formGroup.get('confirmaPassword')?.value;
+
+    // Si las contraseñas no coinciden, devuelve un error, de lo contrario, null
+    return password === confirmaPassword ? null : { passwordsMismatch: true };
+  }
+
+  // Método que se ejecuta al enviar el formulario
+  public registrar() {
+    if (this.registroForm.valid) {
+      console.log(this.registroForm.value);
+      // Lógica para registrar el cliente
+    } else {
+      console.log('El formulario no es válido');
+    }
+  }
+}
