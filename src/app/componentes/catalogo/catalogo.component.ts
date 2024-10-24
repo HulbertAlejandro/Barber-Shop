738c359229ade7bebecf963482c390c8d7089969
Juan Esteban Cruz <juane.cruzp@uqvirtual.edu.co>
2024-10-23 22:56:28 -0500

    Update catalogo

--- src/app/componentes/catalogo/catalogo.component.ts
+++ src/app/componentes/catalogo/catalogo.component.ts
@@ -19,7 +19,7 @@ export class CatalogoComponent {
     id: 2,
     nombre: 'Peinado',
     descripcion: 'Peinado especial para cualquier ocasión.',
-    imagen: 'https://cdn.pixabay.com/photo/2019/02/25/13/45/barbershop-4019683_1280.jpg',
+    imagen: 'https://cdn.pixabay.com/photo/2016/11/21/16/08/tying-hair-1846171_1280.jpg',
     precio: 20
   };
 
@@ -27,7 +27,7 @@ export class CatalogoComponent {
     id: 3,
     nombre: 'Maquillaje',
     descripcion: 'Maquillaje profesional para eventos.',
-    imagen: 'https://cdn.pixabay.com/photo/2019/02/25/13/45/barbershop-4019683_1280.jpg',
+    imagen: 'https://cdn.pixabay.com/photo/2019/11/22/18/21/cosmetics-4645407_1280.jpg',
     precio: 25
   };
 
@@ -35,7 +35,7 @@ export class CatalogoComponent {
     id: 4,
     nombre: 'Manicura',
     descripcion: 'Cuidado de uñas y manos.',
-    imagen: 'https://cdn.pixabay.com/photo/2019/02/25/13/45/barbershop-4019683_1280.jpg',
+    imagen: 'https://cdn.pixabay.com/photo/2017/08/06/00/41/people-2587157_960_720.jpg',
     precio: 10
   };
 }
